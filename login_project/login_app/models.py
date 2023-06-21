7bd8354f3133440faaf73b06b1cd07fde5e43764
shanu G <shanug@shanus-MacBook-Pro.local>
2023-06-22 05:06:18 +0530

    finalized commit

--- login_project/login_app/models.py
+++ login_project/login_app/models.py
@@ -4,7 +4,9 @@ class User(models.Model):
     username = models.CharField(max_length=100)
     password = models.CharField(max_length=100)
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
     content = models.TextField()
-    author = models.CharField(max_length=50)
-    tags = models.CharField(max_length=100)
+    author = models.CharField(max_length=255)
+    tags = models.CharField(max_length=255)
+    core_categories = models.CharField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
